badcdc7e9d15c6f54a9d76ff38a77b6bc66163af
Blond <vsmokx@gmail.com>
2023-05-11 11:37:46 +0200

    🛠️ env cfg

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');//importation de cors
 const mongoose = require('mongoose'); // apres linstall import mongoose
 const userRouter = require('./routes/userRouter') //import de userRouter
 const articleRouter = require('./routes/articleRouter')
-
+require('dotenv').config()
 
 const app = express();// Declaration de notre app express
 app.use(cors()); // Autorise l'application a utiliser Cors
@@ -13,13 +13,18 @@ app.use(articleRouter);
 
 
 //ecoute du server sur le port 3000
-app.listen(3000, (err) => {
+app.listen(process.env.PORT, (err) => {
     if (err) {
         console.log(err);
     } else {
-        console.log('connecteé au serveur sur le port 3000');
+        console.log(`connecteé au serveur sur le port : ${process.env.PORT}`);
     }
 });
 
 //connexion a la base de donnée
-mongoose.connect('mongodb://127.0.0.1:27017/API_Blond');
+try {
+    mongoose.connect(process.env.BDD_URI);
+    console.log("Connecté a la BDD");
+} catch (error) {
+    console.log(error);
+}
